2a890761efb146f6fca60aa472d2fd1d84bee848
PavelKrastev-hub <pavel__krustev@abv.bg>
2026-01-17 18:43:10 +0200

    Add express server

---
+++ server/src/app.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import mongoose from 'mongoose';
+
+const app = express();
+
+const url = 'mongodb://localhost:27017';
+
+try {
+    await mongoose.connect(url, {
+        dbName: 'fade-district-varna',
+    });
+
+    console.log('Successfully connected to DB!');
+} catch (error) {
+    console.error('Cannot connetct to DB', error.message);
+}
+app.listen(3030, () => console.log('Server is listening on http://localhost:3030...'));
