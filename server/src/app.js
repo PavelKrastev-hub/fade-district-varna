e0a731c3a7079a023c519ef4424f9ec01b750077
PavelKrastev-hub <pavel__krustev@abv.bg>
2026-01-19 15:03:01 +0200

    Add jwt token and test login and register actions

--- server/src/app.js
+++ server/src/app.js
@@ -1,11 +1,18 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import dotenv from 'dotenv';
 
-import routes from './routes.js';
+import authRoutes from './routes/authRoute.js';
 
 const app = express();
 
 const url = 'mongodb://localhost:27017';
+
+dotenv.config();
+
+app.use(express.json());
 
 try {
     await mongoose.connect(url, {
@@ -17,5 +24,15 @@ try {
     console.error('Cannot connetct to DB', error.message);
 }
 
-app.use(routes);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+});
+
+app.use('/auth', authRoutes);
+
 app.listen(3030, () => console.log('Server is listening on http://localhost:3030...'));
