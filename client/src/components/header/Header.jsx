36c5e6959d6fb0119b697fcb7936e13c0fcc7617
PavelKrastev-hub <pavel__krustev@abv.bg>
2026-01-23 15:47:15 +0200

    Updating home page html and css

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,34 +1,35 @@
 export default function Header() {
     return (
         <header className="header">
-            <div className="logo">
-                <a href="/">
-                    <span>FADE</span>
-                    <span>✂️</span>
-                    <span>DISTRICT</span>
-                </a>
-            </div>
-
-            <nav>
-                <ul className="navigation">
-                    <li className="item">
-                        <a href="/">Home</a>
-                    </li>
-                    <li className="item">
-                        <a href="/services">Services</a>
-                    </li>
-                    <li className="item">
-                        <a href="/book">Book</a>
-                    </li>
-                    <li className="item">
-                        <a href="/contact">Contact</a>
-                    </li>
-                </ul>
-                <div className="buttons">
-                    <button className="btn">Login</button>
-                    <button className="btn">Register</button>
+            <div className="left-side">
+                <div className="logo">
+                    <a href="/">
+                        <span>FADE</span>
+                        <span>✂️</span>
+                        <span>DISTRICT</span>
+                    </a>
                 </div>
-            </nav>
+                <nav className="navigation">
+                    <ul>
+                        <li className="item">
+                            <a href="/">Home</a>
+                        </li>
+                        <li className="item">
+                            <a href="/services">Services</a>
+                        </li>
+                        <li className="item">
+                            <a href="/book">Book</a>
+                        </li>
+                        <li className="item">
+                            <a href="/contact">Contact</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+            <div className="buttons">
+                <button className="btn login-btn">Login</button>
+                <button className="btn register-btn">Register</button>
+            </div>
         </header>
 
     );
--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -2,15 +2,46 @@ export default function Home() {
     return (
         <main className="home">
             <section className="text">
-                <h1>Style your best look!</h1>
-                <h2>Professional Barbershop!</h2>
+                <h1>
+                    <span className="word-white">Style </span>
+                    <span className="word-orange">your </span>
+                    <span className="word-white">best </span>
+                    <span className="word-orange">look!</span>
+                </h1>
+                <h2 className="">Professional Barbershop!</h2>
 
                 <div className="buttons">
-                    <button>Book Now</button>
-                    <button>Learn More</button>
+                    <button className="btn book-now">Book Now</button>
+                    <button className="btn learn-more">Learn More</button>
+                </div>
+            </section>
+
+            <section class="our-services">
+                <div className="text-in-left">
+                    <p class="section-subtitle">OUR SERVICES</p>
+                    <h2 class="section-title">WHAT WE OFFER</h2>
+                </div>
+                <ul class="services-list">
+                    <li class="service-item">
+                        <img src="./images/clipper.png" alt="Haircut" />
+                        <h3 class="service-title">Haircuts</h3>
+                        <p class="service-desc">Modern & Classic Styles</p>
+                    </li>
+                    <li class="service-item">
+                        <img src="images/service2.png" alt="Shaving" />
+                        <h3 class="service-title">Shaves</h3>
+                        <p class="service-desc">Hot Towel Shave</p>
+                    </li>
+                    <li class="service-item">
+                        <img src="images/beard.png" alt="Styling" />
+                        <h3 class="service-title">Beard Trimming</h3>
+                        <p class="service-desc">Expert Grooming</p>
+                    </li>
+                </ul>
+                <div class="services-btn">
+                    <button class="btn view-services">View All Services</button>
                 </div>
             </section>
         </main>
-
     );
 }
