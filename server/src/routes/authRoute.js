008dc18eb69a6e0931e10407e25a9fcf0c71b2cb
PavelKrastev-hub <pavel__krustev@abv.bg>
2026-02-02 13:12:33 +0200

    Change links for login and register

--- server/src/routes/authRoute.js
+++ server/src/routes/authRoute.js
@@ -3,8 +3,8 @@ import { register, login } from '../controllers/authController.js';
 
 const router = express.Router();
 
-router.post('/register', register);
+router.post('/users/register', register);
 
-router.post('/login', login);
+router.post('/users/login', login);
 
 export default router;
