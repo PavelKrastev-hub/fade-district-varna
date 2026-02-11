a4ca7492865eede5a77969de196e4c081de82b93
PavelKrastev-hub <pavel__krustev@abv.bg>
2026-02-11 19:19:39 +0200

    Success register and save to db

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import Register from "./components/register/Register.jsx";
 import Login from "./components/login/Login.jsx";
 import Appointment from "./components/appointment/Appointment.jsx";
 
+
 // import UserContext from "./contexts/UserContext.jsx";
 // import { useContext } from "react";
 
